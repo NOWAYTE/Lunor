b8e3aa56d3a011102622e86920a853529a4c25aa
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2025-10-28 23:56:29 +0300

    Update

--- graphArbitrage/frontend/src/app/page.tsx
+++ graphArbitrage/frontend/src/app/page.tsx
@@ -7,6 +7,7 @@ import {
   MARKET_DATA_WIDGET_CONFIG,
   MARKET_OVERVIEW_WIDGET_CONFIG,
   TOP_STORIES_WIDGET_CONFIG,
+  ADVANCED_CHART_WIDGET_CONFIG,
 } from "~/lib/constants"
 import CallToAction from "~/components/landing/call-to-action"
 import LandingPageNavbar from "~/components/landing/navbar"
@@ -20,13 +21,19 @@ export default async function Page() {
   const scriptUrl =
     "https://s3.tradingview.com/external-embedding/embed-widget-"
 
-  const cardClass = "" 
+  const cardClass = "scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200" 
 
   return (
     <div className="flex min-h-screen flex-col gap-10 py-8">
-      <section className="container">
+      <section className="container h-screen">
         <LandingPageNavbar />
         <CallToAction />
+         <TradingViewWidget
+          scriptUrl={`${scriptUrl}advanced-chart.js`}
+          config={ADVANCED_CHART_WIDGET_CONFIG("OANDA:EURUSD")}
+          className={cardClass}
+          height={1000}
+        />
       </section>
       <section className="container grid grid-cols-1 gap-8 xl:grid-cols-3">
         <TradingViewWidget
